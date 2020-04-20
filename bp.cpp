41b5a0edfb0b3033f4d7681bad82f8678a4ea2b7
DanielShkolnik <DanielShkolnik@users.noreply.github.com>
2020-04-20 19:15:26 +0300

    new

--- bp.cpp
+++ bp.cpp
@@ -157,6 +157,16 @@ public:
 																		 history(isGlobalHist ,historySize ,btbSize),
 																		 pc(0) ,tagSize(tagSize) ,shared(shared) ,btbSize(btbSize) {
 	}
+
+	bool doesExist(uint32_t pc){
+		int btbRow=indx(pc,this->btbSize);
+		int tag = pc << (32 -(2 + (int)log2(this->btbSize) + this->tagSize));
+		tag = tag >> (2 + (int)log2(this->btbSize));
+		if(this->btb.btb[btbRow].tag == tag) return true;
+		return false;
+
+	}
+
 };
 
 static BranchPredictor* bp = nullptr;
